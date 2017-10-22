f817b72bf5f7fb9b2173674040599c856a052b0d
Michal2017 <33008033+Michal2017@users.noreply.github.com>
2017-10-22 21:52:55 +0200

    Add files via upload

---
+++ Solution1/HelloWorld/MyClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class MyClass
+    {
+        private string text;
+
+        public MyClass(string text)
+        {
+            this.text = text;
+        }
+
+        public void printText()
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ Solution1/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass alwaysTalking = new MyClass("Hello World!");
+
+            alwaysTalking.printText();
+
+            Console.ReadKey();
+        }
+    }
+}
